3aa54de995881d4121226092f0a47ff8efa592d4
Penny Bentley <penny.bentley@cox.net>
2019-10-09 19:21:17 +0000

    lesson 5.16

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,11 @@
 class CoursesController < ApplicationController
 
   def index
-     @courses = Course.all
+    @courses = Course.all
+  end
+
+  def show
+    @course = Course.find(params[:id])
   end
 
 end
