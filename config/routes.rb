30a0176505cdd30496a7ee8086f25dbc3373b3ea
Penny Bentley <penny.bentley@cox.net>
2019-10-20 03:42:30 +0000

    lesson 7.34

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resource :dashboard, only: [:show]
     root 'static_pages#index'
     resources :courses, only: [:index, :show] do
       resources :enrollments, only: :create
@@ -9,10 +10,9 @@ Rails.application.routes.draw do
     resources :lessons, only: [:show]
     namespace :instructor do
       resources :lessons, only: [:update]
-      resources :sections, only: [:update]
-      resources :sections, only: [] do
+      resources :sections, only: [:update] do
         resources :lessons, only: [:new, :create]
-      end
+        end
       resources :courses, only: [:new, :create, :show] do
         resources :sections, only: [:new, :create] do
       end
