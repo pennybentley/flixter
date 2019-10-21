5cb65fd3be0d5e84d39866ee183b967863c2f558
Penny Bentley <penny.bentley@cox.net>
2019-10-21 18:32:26 +0000

    Lesson 8.0 testing on heroku 1.0

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,19 +3,21 @@ class EnrollmentsController < ApplicationController
 
   def create
      # Amount in cents
-    @amount = (current_course.cost * 100).to_i
+    if current_course.premium?
+      @amount = (current_course.cost * 100).to_i
 
-    customer = Stripe::Customer.create(
-      email: params[:stripeEmail],
-      source: params[:stripeToken]
-    )
+      customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: params[:stripeToken]
+      )
 
-    charge = Stripe::Charge.create(
-      customer: customer.id,
-      amount: @amount,
-      description: 'Penny Flixter Class',
-      currency: 'usd'
-    )
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: @amount,
+        description: 'Penny Flixter Class',
+        currency: 'usd'
+      )
+    end
     current_user.enrollments.create(course: current_course)
     redirect_to course_path(current_course)
   rescue Stripe::CardError => e
